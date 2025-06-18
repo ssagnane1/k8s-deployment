4cd2a14ca05c891591389effd2f8bb03d2e0edd6
ssagnane1 <115085357+ssagnane1@users.noreply.github.com>
2025-06-18 21:13:16 +0200

    Delete ansible/join_command directory

--- ansible/join_command/join.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo kubeadm join 172.17.1.4:6443 --token sa7xb3.0x8uibsm93f5pu1u --discovery-token-ca-cert-hash sha256:9b612044ae01bdd30bb6e778a0031e7c807239c6ca3b28a81b43c3b57dd68f9a 
