8b160b91ea1c0ae54fbded5477b8d4cb6a29c317
Saidou <ssagnane1@myges.fr>
2025-06-19 00:17:03 +0000

    Update lyvs v3

--- ansible/join_command/join.sh
+++ ansible/join_command/join.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo kubeadm join 172.17.1.4:6443 --token sa7xb3.0x8uibsm93f5pu1u --discovery-token-ca-cert-hash sha256:9b612044ae01bdd30bb6e778a0031e7c807239c6ca3b28a81b43c3b57dd68f9a 
+sudo kubeadm join 172.17.1.4:6443 --token homnvb.p0dayxqun8x6phbh --discovery-token-ca-cert-hash sha256:2a8601849d78160beb59a9075baea79628ec03aae3d892bb56d69278cc886391 
